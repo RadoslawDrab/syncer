1770aa2a811dae6b7d21a4e6f3684c12bb557192
Radosław Drab <radoslaw.drab03@gmail.com>
2023-10-19 20:09:08 +0200

    added playlists endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,14 @@ import express from 'express'
 
 import userRouter from 'routers/users'
 import songsRouter from 'routers/songs'
+import playlistsRouter from 'routers/playlists'
 
 const app = express()
 const port = process.env['PORT'] || 3000
 
 app.use('/users', userRouter)
 app.use('/songs', songsRouter)
+app.use('/playlists', playlistsRouter)
 
 app.listen(port, () => {
 	console.clear()
