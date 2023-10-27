b6ce0760d2f4bbec3d16ace7b953fbd7084a629a
Radosław Drab <radoslaw.drab03@gmail.com>
2023-10-28 01:08:18 +0200

    added signing in endpoint

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,5 +1,8 @@
 import { Response, Request } from 'express'
 
+import { getUser as getFirebaseUser } from 'src/auth'
+import { setError, setStatus } from 'src/utils'
+
 export function getUsers(req: Request, res: Response) {
 	res.status(200).json({ status: 'users' })
 }
@@ -14,4 +17,36 @@ export function updateUser(req: Request, res: Response) {
 }
 export function deleteUser(req: Request, res: Response) {
 	res.status(200).json({ status: 'deleted' })
+}
+export async function signInUser(req: Request, res: Response) {
+	try {
+		const token = req.headers.token
+		if (!token || typeof token !== 'string') {
+			setError(res, {
+				code: 400,
+				message: 'No token value'
+			})
+			return
+		}
+		const user = await getFirebaseUser(token)
+		setStatus(
+			res,
+			{
+				code: 200,
+				message: 'User successfully signed in'
+			},
+			{ user }
+		)
+	} catch (error) {
+		setError(res, {
+			code: error.code || 400,
+			message: error.message || 'Bad request'
+		})
+	}
+}
+export function signUpUser(req: Request, res: Response) {
+	res.status(200).json({ status: 'signed up' })
+}
+export function logOutUser(req: Request, res: Response) {
+	res.status(200).json({ status: 'logged out' })
 }
--- server/src/routers/users.ts
+++ server/src/routers/users.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 
-import { addUser, deleteUser, getUser, getUsers, updateUser } from 'controllers/users'
+import { addUser, deleteUser, getUser, getUsers, signInUser, updateUser } from 'controllers/users'
 import { checkUser } from 'middleware/index'
 
 const router = Router()
 
 router.route('/').get(getUsers).post(addUser)
+router.route('/auth').get(signInUser)
 router.route('/:id').get(getUser)
 
 router.use(checkUser)
@@ -17,6 +18,8 @@ router.route('/:id').patch(updateUser).delete(deleteUser)
   Routes:
   - /
     GET, POST
+  - /auth
+    GET
   - /:id
     GET, PATCH, DELETE
 */
