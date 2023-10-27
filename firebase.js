4b642d34daf8a2b2d9d9a3120c4865ac5ca7d53d
Radosław Drab <radoslaw.drab03@gmail.com>
2023-10-28 00:58:30 +0200

    added firebase config file

---
+++ firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+	apiKey: 'AIzaSyDDGoO-EMrKcoDloOMunw8vA8_7i9HjZbA',
+	authDomain: 'syncer-d8d4f.firebaseapp.com',
+	projectId: 'syncer-d8d4f',
+	storageBucket: 'yncer-d8d4f.appspot.com',
+	messagingSenderId: '220525688709',
+	appId: '1:220525688709:web:d1aba7841b97d7167f36ec'
+}
