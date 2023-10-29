e6a29189d4b9f1620949986aa0301a990396f433
Radosław Drab <radoslaw.drab03@gmail.com>
2023-10-29 19:20:44 +0100

    added users middleware

--- server/src/routers/users.ts
+++ server/src/routers/users.ts
@@ -2,18 +2,19 @@ import { Router } from 'express'
 
 import { deleteUser, getUser, getUsers, signInUser, signOutUser, updateUser } from 'controllers/users'
 import { checkUser } from 'middleware/index'
+import { checkToken, checkBody } from 'middleware/users'
 
 const router = Router()
 
 router.route('/').get(getUsers)
 router.route('/:id').get(getUser)
 
-router.route('/auth/sign-in').get(signInUser)
-router.route('/auth/sign-out').get(signOutUser)
+router.route('/auth/sign-in').get(checkToken, signInUser)
+router.route('/auth/sign-out').get(checkToken, signOutUser)
 
 router.use(checkUser)
 
-router.route('/:id').patch(updateUser).delete(deleteUser)
+router.route('/:id').patch(checkBody, updateUser).delete(deleteUser)
 
 /*
   Base route: /users
