b6add5b366605f330da0d071a9967a28e21e5726
Radosław Drab <radoslaw.drab03@gmail.com>
2023-10-29 16:45:50 +0100

    modified user routes

--- server/src/routers/users.ts
+++ server/src/routers/users.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express'
 
-import { addUser, deleteUser, getUser, getUsers, signInUser, updateUser } from 'controllers/users'
+import { addUser, deleteUser, getUser, getUsers, signInUser, signOutUser, updateUser } from 'controllers/users'
 import { checkUser } from 'middleware/index'
 
 const router = Router()
 
 router.route('/').get(getUsers).post(addUser)
-router.route('/auth').get(signInUser)
 router.route('/:id').get(getUser)
+
+router.route('/auth/sign-in').get(signInUser)
+router.route('/auth/sign-out').get(signOutUser)
 
 router.use(checkUser)
 
@@ -18,7 +20,9 @@ router.route('/:id').patch(updateUser).delete(deleteUser)
   Routes:
   - /
     GET, POST
-  - /auth
+  - /auth/sign-in
+    GET
+  - /auth/sign-out
     GET
   - /:id
     GET, PATCH, DELETE
