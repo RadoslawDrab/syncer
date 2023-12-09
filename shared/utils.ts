17e4186c1abdb66d1d2ba9f493d809e7f59feb2e
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-09 17:47:35 +0100

    added findObject function

--- shared/utils.ts
+++ shared/utils.ts
@@ -1,8 +1,16 @@
+import { ObjectKeys } from 'shared/types/global'
+
 export function filterObject<Value extends any>(
-	obj: { [key: string]: Value },
-	filterCallback: (value: Value) => boolean
-): { [key: string]: Value } {
-	return Object.keys(obj).reduce((prev, key) => {
-		return filterCallback(obj[key] as Value) ? { ...prev, [key]: obj[key] } : prev
+	obj: ObjectKeys<Value>,
+	func: (value: Value, index: number) => boolean
+): ObjectKeys<Value> {
+	return Object.keys(obj).reduce((prev, key, i) => {
+		return func(obj[key] as Value, i) ? { ...prev, [key]: obj[key] } : prev
 	}, {})
+}
+export function findObject<Value extends any>(
+	obj: ObjectKeys<Value>,
+	func: (value: Value, index: number) => boolean
+): Value | null {
+	return obj[Object.keys(obj).find((key, i) => func(obj[key] as Value, i)) ?? -1] || null
 }
