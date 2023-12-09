f8d2140dbbe6e08d58b12d47cae07ce8f85ad25f
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-09 18:26:15 +0100

    added songs endpoint

--- server/src/controllers/songs.ts
+++ server/src/controllers/songs.ts
@@ -1,17 +1,21 @@
-import { Request, Response } from 'express'
+import { v4 } from 'uuid'
 
-export function getSongs(req: Request, res: Response) {
-	res.status(200).json({ status: 'songs' })
-}
-export function getSong(req: Request, res: Response) {
-	res.status(200).json({ status: 'song' })
-}
-export function addSong(req: Request, res: Response) {
-	res.status(200).json({ status: 'added' })
-}
-export function updateSong(req: Request, res: Response) {
-	res.status(200).json({ status: 'updated' })
-}
-export function deleteSong(req: Request, res: Response) {
-	res.status(200).json({ status: 'deleted' })
-}
+import { FullSong, Song, SongItem } from 'shared/types/database'
+import { Endpoint } from 'src/utils/classes'
+
+const endpoint = new Endpoint<FullSong>('/songs', 'Song')
+endpoint.setAddCallback<FullSong, SongItem>((data, req) => {
+	return { id: v4(), createdAt: Date.now(), updatedAt: Date.now(), userId: req.query.userId?.toString() ?? '', song: data }
+})
+endpoint.setUpdateCallback<SongItem, SongItem>((data) => {
+	return { ...data, updatedAt: Date.now() }
+})
+endpoint.setUpdateBodyCallback<SongItem, SongItem, Partial<Song>>((data, body) => {
+	return { ...data, song: { ...data.song, ...body } }
+})
+
+export const getSong = endpoint.getSingle
+export const getSongs = endpoint.getAll
+export const addSong = endpoint.add
+export const updateSong = endpoint.update
+export const deleteSong = endpoint.delete
