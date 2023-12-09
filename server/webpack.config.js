a49e22e0b4800e61dac70f20e36a7c18e2186591
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-09 17:27:28 +0100

    added shared import path

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -2,10 +2,17 @@ const tsconfig = require('./tsconfig.json')
 
 const { paths, baseUrl } = tsconfig['compilerOptions']
 const aliases = Object.keys(paths).reduce((obj, pathKey) => {
-	const starRegex = /\/\*/g
+	const starRegex = /\/\*/g // /*
+	const currentPathRegex = /(?<!\.)\.\// // ./
+	const lastSlashRegex = /\/?$/g // e.g. src/
+
+	const basePath = baseUrl.replace(currentPathRegex, '').replace(lastSlashRegex, '')
 	const alias = pathKey.replace(starRegex, '')
-	const path = paths[pathKey][0].replace(starRegex, '').replace('./', baseUrl.replace('./', ''))
-	return { ...obj, [alias]: `${__dirname}/${path}/` }
+
+	const path = paths[pathKey][0].replace(starRegex, '').replace(currentPathRegex, '')
+
+	const resolvePath = `${basePath}/${path}/`
+	return { ...obj, [alias]: `${__dirname}/${resolvePath}` }
 }, {})
 
 const config = {
