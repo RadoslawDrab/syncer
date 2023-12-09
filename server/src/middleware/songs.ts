38fc276774b432293aec23bd7608484ab67c97c7
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-09 18:25:59 +0100

    added songs endpoint middleware

---
+++ server/src/middleware/songs.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express'
+
+import { KeysType, containsPartialType, containsType } from '.'
+
+import { Song } from 'shared/types/database'
+
+const songKeys: KeysType<Song> = {
+	mandatory: [
+		'name',
+		'artist',
+		'album',
+		'releaseDate',
+		'genres',
+		'isExplicit',
+		'youtubeUrl',
+		'lyricOffset',
+		'trimStart',
+		'trimEnd'
+	],
+	optional: ['lyrics']
+}
+
+export function containsSong(req: Request, res: Response, next: NextFunction) {
+	containsType.call(songKeys, req, res, next)
+}
+export function containsPartialSong(req: Request, res: Response, next: NextFunction) {
+	containsPartialType.call(songKeys, req, res, next)
+}
--- server/src/routers/songs.ts
+++ server/src/routers/songs.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express'
 
 import { addSong, deleteSong, getSong, getSongs, updateSong } from 'controllers/songs'
-import { checkSong } from 'middleware/index'
+import { checkBody, checkId, checkSong } from 'middleware/index'
+import { containsPartialSong, containsSong } from 'middleware/songs'
 
 const router = Router()
 
-router.route('/').get(getSongs).post(addSong)
-router.route('/:id').get(getSong)
+router.route('/').get(getSongs).post(containsSong, checkBody, addSong)
+router.route('/:id').get(checkId, getSong)
 
 router.use(checkSong)
 
-router.route('/:id').patch(updateSong).delete(deleteSong)
+router.route('/:id').patch(checkId, checkBody, containsPartialSong, updateSong).delete(checkId, deleteSong)
 
 /*
   Base route: /songs
