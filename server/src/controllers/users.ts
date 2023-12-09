560230efeb248b216d2734febc4d2cabeb0c3575
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-09 20:06:12 +0100

    added users endpoint

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -3,59 +3,32 @@ import { Response, Request } from 'express'
 
 import { getUser as getFirebaseUser, getJwtToken } from 'auth/index'
 import { setError, setStatus } from 'utils'
-import { removeData, updateData } from 'src/config/firebase'
+import { Endpoint } from 'utils/classes'
 
-import { RequestBody } from 'src/types/server'
+import { FullUser as DBFullUser } from 'shared/types/database'
 
 const auth = getAuth()
 
-export function getUsers(req: Request, res: Response) {
-	res.status(200).json({ status: 'users' })
-}
-export function getUser(req: Request, res: Response) {
-	res.status(200).json({ status: 'user' })
-}
-export function updateUser(req: Request, res: Response) {
-	const user = auth.currentUser as User
-	const body: RequestBody = req.body
+const endpoint = new Endpoint<DBFullUser>('users/', 'User')
 
-	updateCurrentUser(auth, {
-		...user,
-		...body
-	})
-	updateData(`users/${user.uid}`, {
-		...body
-	})
-
-	setStatus(
-		res,
-		{
-			code: 200,
-			message: 'User updated'
-		},
-		null
-	)
-}
-export async function deleteUser(req: Request, res: Response) {
-	try {
-		const user = auth.currentUser
-		await removeData(`users/${user?.uid}`)
-		await user?.delete()
-		setStatus(
-			res,
-			{
-				code: 200,
-				message: 'User deleted'
-			},
-			null
-		)
-	} catch (error) {
-		setError(res, {
-			code: 500,
-			message: error.message || 'Internal server error'
-		})
+endpoint.setUpdateBodyCallback((data, body, req) => {
+	if (!req.headers['dev-mode']) {
+		const user = auth.currentUser as User
+		updateCurrentUser(auth, { ...user, ...req.body })
 	}
-}
+	return body
+})
+endpoint.setDeleteCallback(async (data) => {
+	const user = auth.currentUser
+	await user?.delete()
+	return data
+})
+
+export const getUsers = endpoint.getAll
+export const getUser = endpoint.getSingle
+export const updateUser = endpoint.update
+export const deleteUser = endpoint.delete
+
 export async function signInUser(req: Request, res: Response) {
 	const token = req.headers.token
 	const isIdToken = Boolean(req.headers['is-id-token'])
