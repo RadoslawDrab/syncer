f2b3ee7d10e93c7dd3169a3985d29c959d2e4153
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-17 22:07:09 +0100

    added paths config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { AliasOptions, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tsconfig from './tsconfig.app.json'
 
+const alias = tsToViteAliases(tsconfig.compilerOptions.baseUrl, tsconfig.compilerOptions.paths)
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias
   }
 })
+
+function tsToViteAliases(baseUrl: string, paths: PathsObject): AliasOptions {
+  return Object.keys(paths).reduce((acc, pathKey) => {
+    const path: string[] = paths[pathKey]
+    const starRegex = /\/\*/g
+
+    const pathAliases = path.map((p) =>
+      fileURLToPath(new URL(baseUrl + p.replace(starRegex, ''), import.meta.url))
+    )
+    return {
+      ...acc,
+      [pathKey.replace(starRegex, '')]: pathAliases
+    }
+  }, {})
+}
+type PathsObject = { [key: string]: string[] }
