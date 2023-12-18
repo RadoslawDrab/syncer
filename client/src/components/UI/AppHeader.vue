3221bcf44e0647c310f1319ae8a4a3971579fb4c
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-18 11:18:06 +0100

    removed unnecessary code

---
+++ client/src/components/UI/AppHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { PhHeadphones } from '@phosphor-icons/vue'
+
+</script>
+
+<template>
+  <header class="px-4 py-2 w-full flex justify-between gap-1">
+    <PhHeadphones size="30" />
+    <button class="button">Test</button>
+    <nav class="flex justify-between gap-2">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/">Library</RouterLink>
+      <RouterLink to="/">Sign Up</RouterLink>
+      <RouterLink to="/">Sign In</RouterLink>
+    </nav>
+  </header>
+</template>
