944f5774d6f8d55619f8be2bcff4f70b909b122f
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-20 12:58:54 +0100

    added header link

---
+++ client/src/components/UI/AppHeaderLink.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+const props = defineProps<{to: string}>()
+
+const route = useRoute()
+const className = computed(() => {
+  const isActive = route.path === props.to 
+  return `btn btn-border-bottom rounded-none btn-outline-${isActive ? 'primary' : 'secondary'} ${isActive ? 'btn-text-inherit': ''}`
+})
+</script>
+<template>
+  <RouterLink :class="className" :to="props.to">
+    <slot>Link</slot>
+  </RouterLink>
+</template>
