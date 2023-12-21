ffa42e0f9cc42491d3aec95ee9db9f343383e020
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-21 17:34:48 +0100

    separated vue and scss

--- client/src/components/Layout/LayoutHeader.vue
+++ client/src/components/Layout/LayoutHeader.vue
@@ -65,31 +65,5 @@ useEventListener(window, 'resize', (event) => {
 </template>
 
 <style scoped lang="scss">
-header {
-  @apply sticky top-0 left-0 px-4 py-3 mb-5 flex justify-between items-center gap-1 bg-light dark:bg-dark border-secondary-100 dark:border-secondary-900 text-secondary-800 dark:text-secondary-200;
-
-  &::before {
-    @apply content-[''] -z-10 absolute bottom-0 left-1/2 -translate-x-1/2 w-screen h-full bg-inherit border-b-2 border-inherit dark:border-inherit;
-  }
-  & > div {
-    @apply bg-inherit;
-    nav {
-      @apply absolute top-full inset-x-0 w-full p-7 bg-inherit border-b-2 border-inherit flex flex-col items-start gap-2;
-      @apply sm:static sm:h-auto sm:w-auto sm:border-0 sm:p-0 sm:flex-row sm:flex;  
-    }
-  }
-}
-
-.v-enter-active,
-.v-leave-active {
-  @apply transition-all duration-500;
-}
-.v-leave-from,
-.v-enter-to {
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
-}
-.v-enter-from,
-.v-leave-to {
-  clip-path: polygon(0 0, 100% 0, 100% 0, 0 0);
-}
-</style>stores/useTheme
+@import './LayoutHeader.scss';
+</style>
