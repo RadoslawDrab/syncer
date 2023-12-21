12c4fadba83578fbe728ffc3067dbc913379443d
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-21 16:26:38 +0100

    added AppButton

---
+++ client/src/components/UI/AppButton.vue
@@ -0,0 +1,22 @@
+<script setup lang='ts'>
+  import type { ButtonStyle } from 'types/button';
+  
+  const props = defineProps<AppButtonProps>()
+
+  const buttonClassName = ['btn', props.buttonStyle && props.buttonStyle.type !== 'default' ? `btn-${props.buttonStyle.type}-${props.buttonStyle.color}` : '']
+
+  interface AppButtonProps {
+    buttonStyle?: ButtonStyle
+  }
+
+</script>
+
+<template>
+  <button :class="buttonClassName">
+    <slot>Button</slot>
+  </button>
+</template>
+
+<style scoped lang='scss'>
+  
+</style>
