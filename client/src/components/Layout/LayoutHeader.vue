515a3a982ed04261ec435a13ae4df8f7e0dd23f3
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-21 17:34:26 +0100

    modified header

--- client/src/components/Layout/LayoutHeader.vue
+++ client/src/components/Layout/LayoutHeader.vue
@@ -32,33 +32,35 @@ useEventListener(window, 'resize', (event) => {
 
 <template>
   <header>
-    <div class="flex items-center gap-4">
-    <button @click="router.push('/')">
-      <PhHeadphones size="30" class="fill-primary-500" />
-    </button>
-    <Transition>
-      <nav v-show="!navIsHidden">
-        <LayoutHeaderLink v-for="route in routes" :key="route.name" :to="route.path">{{ capitalizeFirstLetter(route.name?.toString() ?? '') }}</LayoutHeaderLink>
-      </nav>
-    </Transition>
-    <button class="btn btn-primary" @click="theme.toggle">{{ capitalizeFirstLetter(theme.value) }}</button>
-    <button :class="['sm:hidden btn btn-transparent-primary', navIsHidden ? 'text-inherit' : '']" @click="toggleNavigation">
-      <PhList size="30" class="fill-current" />
-    </button>
-  </div>
-  <AppDropdown content-position="left-bottom">
-    <template #button>
-      <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-    </template>
-    <template #content>
-      <div class="flex flex-col gap-2 p-3 bg-secondary-100 border-2 border-secondary-200">
-        <button class="btn btn-transparent-secondary w-max">Sign In</button>
-        <button class="btn btn-transparent-secondary w-max">Sign Up</button>
-        <button class="btn btn-transparent-secondary w-max">Account</button>
-        <button class="btn btn-transparent-secondary w-max">Sign Out</button>
-      </div>
-    </template>
-  </AppDropdown>
+    <div>
+      <button @click="router.push('/')">
+        <PhHeadphones size="30" class="fill-primary-500" />
+      </button>
+      <Transition>
+        <nav v-show="!navIsHidden">
+          <LayoutHeaderLink v-for="route in routes" :key="route.name" :to="route.path">{{ capitalizeFirstLetter(route.name?.toString() ?? '') }}</LayoutHeaderLink>
+        </nav>
+      </Transition>
+      <AppButton :button-style="{ color: 'primary'}" @click="theme.toggle">
+        {{ capitalizeFirstLetter(theme.value) }}
+      </AppButton>
+      <AppButton :button-style="{ type: 'transparent', color: 'primary'}" :class="['sm:hidden', navIsHidden ? 'text-inherit' : '']" @click="toggleNavigation">
+        <PhList size="30" class="fill-current" />
+      </AppButton>
+    </div>
+    <AppDropdown content-position="left-bottom">
+      <template #button>
+        <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+      </template>
+      <template #content>
+        <div class="flex flex-col gap-2 p-3 bg-secondary-100 dark:bg-secondary-900 border-secondary-200 dark:border-secondary-800">
+          <button class="btn btn-transparent-secondary w-max">Sign In</button>
+          <button class="btn btn-transparent-secondary w-max">Sign Up</button>
+          <button class="btn btn-transparent-secondary w-max">Account</button>
+          <button class="btn btn-transparent-secondary w-max">Sign Out</button>
+        </div>
+      </template>
+    </AppDropdown>
   </header>
 </template>
 
