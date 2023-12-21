650cd331614f8e6c19d6ac1b8203d9989e069f22
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-21 11:20:52 +0100

    removed unnecessary position types

--- shared/types/global.ts
+++ shared/types/global.ts
@@ -1,5 +1,1 @@
 export type ObjectKeys<T> = { [key: string]: T }
-
-export type VerticalPosition = 'top' | 'center' | 'bottom'
-export type HorizontalPosition = 'left' | 'center' | 'right'
-export type Position = `${VerticalPosition}-${HorizontalPosition}`
