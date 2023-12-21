5be27ba94262028961e06e6bab2cd62e05898bee
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-21 11:14:32 +0100

    added utils folder

--- server/src/utils/classes.ts
+++ server/src/utils/classes.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express'
 import { setError, setStatus } from '.'
 
 import { getData, setData } from 'config/firebase'
-import { compareArrays, filterObject, findObject, flattenObject, objectToArray } from 'shared/utils'
+import { compareArrays, filterObject, findObject, flattenObject, objectToArray } from 'shared/utils/object'
 import { ObjectKeys } from 'shared/types/global'
 
 export class Endpoint<Type extends { id: string }> {
