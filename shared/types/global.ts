93f9f0e893c466ccb8046ca6819591546a57594e
Radosław Drab <radoslaw.drab03@gmail.com>
2023-12-21 11:03:55 +0100

    added position types

--- shared/types/global.ts
+++ shared/types/global.ts
@@ -1,1 +1,5 @@
 export type ObjectKeys<T> = { [key: string]: T }
+
+export type VerticalPosition = 'top' | 'center' | 'bottom'
+export type HorizontalPosition = 'left' | 'center' | 'right'
+export type Position = `${VerticalPosition}-${HorizontalPosition}`
